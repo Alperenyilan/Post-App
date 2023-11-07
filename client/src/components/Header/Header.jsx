f7aa0f0ce093034546c554d886532adfe8908369
alperen <alperenyln1@gmail.com>
2023-11-07 22:51:48 +0300

    Header Component Son Duzenlemeler

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,46 +1,75 @@
-import { Input } from "antd";
-import { SearchOutlined, HomeOutlined } from "@ant-design/icons";
+import { Input, Badge } from "antd";
+import {
+  SearchOutlined,
+  HomeOutlined,
+  ShoppingCartOutlined,
+  CopyOutlined,
+  UserOutlined,
+  BarChartOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
 const Header = () => {
   return (
-    <div className="border-b mb-6">
-      <header className="py-4 px-6 flex justify-between items-center gap-10">
+    <div className="mb-6 border-b">
+      <header className="flex items-center justify-between gap-10 px-6 py-4">
         <div className="logo">
           <a href="/">
             <h2 className="text-2xl font-bold md:text-4xl">LOGO</h2>
           </a>
         </div>
-        <div className="header-search flex-1">
+        <div className="flex-1 header-search">
           <Input
             size="large"
-            placeholder="Item Ara..."
+            placeholder="Ürün Ara..."
             prefix={<SearchOutlined />}
-            className="rounded-full max-w-800"
+            className="rounded-full max-w-[800px]"
           />
         </div>
-        <div className="menu-links flex justify-between items-center gap-10">
-          <a href="/" className=" menu-link flex flex-col">
-            <HomeOutlined className="md:text-2xl text-xl" />
-            <span className="md:text-xs text-[10px]">Home</span>
+        <div className="flex items-center justify-between gap-8 menu-links">
+          <a
+            href={"/"}
+            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
+          >
+            <HomeOutlined className="text-xl md:text-2xl" />
+            <span className="md:text-xs text-[10px]">Ana Sayfa</span>
           </a>
-          <a href="/" className=" menu-link flex flex-col">
-            <HomeOutlined className="md:text-2xl text-xl" />
-            <span className="md:text-xs text-[10px]">Home</span>
+          <Badge count={5} offset={[0, 6]} className="hidden md:flex">
+            <a
+              href={"/"}
+              className="menu-link  flex flex-col hover:text-[#40a9ff] transition-all"
+            >
+              <ShoppingCartOutlined className="text-xl md:text-2xl" />
+              <span className="md:text-xs text-[10px]">Sepet</span>
+            </a>
+          </Badge>
+          <a
+            href={"/"}
+            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
+          >
+            <CopyOutlined className="text-xl md:text-2xl" />
+            <span className="md:text-xs text-[10px]">Faturalar</span>
           </a>
-          <a href="/" className=" menu-link flex flex-col">
-            <HomeOutlined className="md:text-2xl text-xl" />
-            <span className="md:text-xs text-[10px]">Home</span>
+
+          <a
+            href={"/"}
+            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
+          >
+            <UserOutlined className="text-xl md:text-2xl" />
+            <span className="md:text-xs text-[10px]">Müşteriler</span>
           </a>
-          <a href="/" className=" menu-link flex flex-col">
-            <HomeOutlined className="md:text-2xl text-xl" />
-            <span className="md:text-xs text-[10px]">Home</span>
+          <a
+            href={"/"}
+            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
+          >
+            <BarChartOutlined className="text-xl md:text-2xl" />
+            <span className="md:text-xs text-[10px]">İstatistikler</span>
           </a>
-          <a href="/" className=" menu-link flex flex-col">
-            <HomeOutlined className="md:text-2xl text-xl" />
-            <span className="md:text-xs text-[10px]">Home</span>
-          </a>
-          <a href="/" className=" menu-link flex flex-col">
-            <HomeOutlined className="md:text-2xl text-xl" />
-            <span className="md:text-xs text-[10px]">Home</span>
+          <a
+            href={"/"}
+            className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
+          >
+            <LogoutOutlined className="text-xl md:text-2xl" />
+            <span className="md:text-xs text-[10px]">Ana Sayfa</span>
           </a>
         </div>
       </header>
