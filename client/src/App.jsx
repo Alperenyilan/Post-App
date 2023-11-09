a362176d6ae513b7dc03655c2571624fe57cee0c
alperen <alperenyln1@gmail.com>
2023-11-09 14:17:59 +0300

    Kategori componenti hazırlık

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
 import Header from "./components/Header/Header";
+import Categories from "./components/categories/Categories";
 
 function App() {
   return (
     <div>
       <Header />
-      home
+      <div className="home px-6 justify-between flex gap-10 ">
+        <div className="categories flex-1 overflow-auto max-h-[calc(100vh-_-112px)]">
+          <Categories />
+        </div>
+        <div className="products flex-[8]">
+          <div className="">products</div>
+        </div>
+        <div className="">
+          <div>cart totals</div>
+        </div>
+      </div>
     </div>
   );
 }
