a4b89a0c7c514984526367fe5296dc77623c4b91
alperen <alperenyln1@gmail.com>
2023-11-17 18:55:38 +0300

    CartTotals css duzenlemeleri

--- client/src/components/cart/CartTotals.jsx
+++ client/src/components/cart/CartTotals.jsx
@@ -1,11 +1,19 @@
 const CartTotals = () => {
   return (
-    <div className="cart">
+    <div className="cart h-full max-h-[calc(100vh_-_90px)] flex flex-col">
       <h2 className="bg-blue-600 text-center py-4 text-white font-bold tracking-wide">
         Sepetteki ürünler
       </h2>
       <div className="cart-items">
         <div className="cart-item">cart item </div>
+      </div>
+      <div className="cart-totals mt-auto  ">
+        <div>
+          <div className="flex justify-between">
+            <b>Ara Toplam</b>
+            <span>99₺</span>
+          </div>
+        </div>
       </div>
     </div>
   );
