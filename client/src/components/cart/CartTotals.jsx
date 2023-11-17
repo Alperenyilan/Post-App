a594f46105a9d1c69c4dc495367809ad13e538b3
alperen <alperenyln1@gmail.com>
2023-11-17 15:34:19 +0300

    Cart totals

---
+++ client/src/components/cart/CartTotals.jsx
@@ -0,0 +1,14 @@
+const CartTotals = () => {
+  return (
+    <div className="cart">
+      <h2 className="bg-blue-600 text-center py-4 text-white font-bold tracking-wide">
+        Sepetteki ürünler
+      </h2>
+      <div className="cart-items">
+        <div className="cart-item">cart item </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartTotals;
