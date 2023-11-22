f8099273c148cbbda6753e4f9139ea335951f248
alperen <alperenyln1@gmail.com>
2023-11-22 21:39:21 +0300

    CartTotals Css degisikler

--- client/src/components/cart/CartTotals.jsx
+++ client/src/components/cart/CartTotals.jsx
@@ -1,5 +1,9 @@
 import { Button } from "antd";
-import { ClearOutlined } from "@ant-design/icons";
+import {
+  ClearOutlined,
+  PlusCircleOutlined,
+  MinusCircleOutlined,
+} from "@ant-design/icons";
 
 const CartTotals = () => {
   return (
@@ -7,9 +11,36 @@ const CartTotals = () => {
       <h2 className="bg-blue-600 text-center py-4 text-white font-bold tracking-wide">
         Sepetteki Ürünler
       </h2>
-      <div className="cart-items">
-        <div className="cart-item">cart item</div>
-      </div>
+      <ul className="cart-items px-2 flex flex-col gap-y-3 pt-2">
+        <li className="cart-item flex justify-between">
+          <div className="flex items-center">
+            <img
+              src="https://www.verita.com.tr/wp-content/uploads/2014/08/elma.jpg"
+              alt=""
+              className="w-16 h-16 object-cover"
+            />
+            <div className="flex flex-col ml-2">
+              <b>Elma</b>
+              <span>12₺ x 2</span>
+            </div>
+          </div>
+          <div className="flex ml-2  items-center">
+            <Button
+              type="primary"
+              size="middle"
+              className="w-full mt-2 flex items-center justify-center !rounded-full"
+              icon={<PlusCircleOutlined />}
+            />
+            <span className="mt-2">1</span>
+            <Button
+              type="primary"
+              size="middle"
+              className="w-full mt-2 flex items-center justify-center !rounded-full"
+              icon={<MinusCircleOutlined />}
+            />
+          </div>
+        </li>
+      </ul>
       <div className="cart-totals mt-auto">
         <div className="border-t border-b">
           <div className="flex justify-between p-2">
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import setupLocatorUI from "@locator/runtime";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+if (process.env.NODE_ENV === "development") {
+  setupLocatorUI();
+}
